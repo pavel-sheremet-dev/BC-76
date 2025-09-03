311d754dfc59d565d1fcc8f76b2bbb5325b94275
Pavlo Sheremet <pashqa.sheremet@gmail.com>
2025-09-03 09:14:06 +0300

    update readme

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import { Metadata } from "next";
 import Section from "@/components/Section/Section";
 import Image from "next/image";
 
-import imageUrl from "@/public/example.jpg";
-
 export const metadata: Metadata = {
   title: "BC-76 | Home Page",
   description: "Home Page Description",
@@ -21,27 +19,11 @@ export default function HomePage() {
         sunt?
       </p>
       <Image
-        // src="/example.jpg"
-
         src="https://picsum.photos/536/354"
         alt="image_alt"
         priority
         width={200}
         height={200}
-      />
-
-      <Image
-        // src="/example.jpg"
-
-        src={imageUrl}
-        alt="image_alt"
-        priority
-        style={{
-          width: "100%",
-          // height: "200px",
-          objectFit: "contain",
-          objectPosition: "center center",
-        }}
       />
     </Section>
   );
